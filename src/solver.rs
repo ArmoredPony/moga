412e2305a951eacdee95512b79896c8a09058c1a
ArmoredPony <raxmetov2003@gmail.com>
2024-08-23 23:58:22 +0400

    fix pareto dominance

--- src/solver.rs
+++ src/solver.rs
@@ -45,24 +45,26 @@ impl<S> SolverResults<S> {
   }
 }
 
-trait ParetoDominance<S> {
+/// Describes pareto dominance for arrays of floats.
+trait ParetoDominance {
+  /// Returns `Less` if `self` dominates `other`, `Greater` if `other`
+  /// dominates `Self`, otherwise `Equal`. `self` dominates `other` if all
+  /// `self` values are closer to zero than respective `other` values.
   fn dominance(&self, other: &Self) -> Ordering;
 }
 
-impl<const N: usize, S: PartialOrd> ParetoDominance<S> for [S; N] {
+impl ParetoDominance for [f32] {
   fn dominance(&self, other: &Self) -> Ordering {
-    self
-      .iter()
-      .zip(other)
-      .find_map(|(a, b)| {
-        a.partial_cmp(b)
-          .map(|ord| match ord {
-            Ordering::Equal => None,
-            ord => Some(ord),
-          })
-          .unwrap_or(Some(Ordering::Equal))
-      })
-      .unwrap_or(Ordering::Equal)
+    let mut ord = Ordering::Equal;
+    for (a, b) in self.iter().zip(other) {
+      match (ord, a.abs().partial_cmp(&b.abs()).expect("NaN encountered")) {
+        (Ordering::Equal, next_ord) => ord = next_ord,
+        (Ordering::Greater, Ordering::Less)
+        | (Ordering::Less, Ordering::Greater) => return Ordering::Equal,
+        _ => {}
+      }
+    }
+    ord
   }
 }
 
@@ -73,6 +75,21 @@ mod tests {
   #[test]
   fn test_pareto_dominance() {
     assert_eq!([1.0, 2.0, 3.0].dominance(&[1.0, 2.0, 3.0]), Ordering::Equal);
+    assert_eq!(
+      [-1.0, 2.0, -3.0].dominance(&[-1.0, 2.0, -3.0]),
+      Ordering::Equal
+    );
+    assert_eq!(
+      [-2.0, 1.0, 3.0].dominance(&[2.0, -1.0, -3.0]),
+      Ordering::Equal
+    );
+
+    assert_eq!([1.0, 2.0, 3.0].dominance(&[3.0, 2.0, 1.0]), Ordering::Equal);
+    assert_eq!(
+      [1.0, -2.0, 3.0].dominance(&[-3.0, 2.0, 1.0]),
+      Ordering::Equal
+    );
+
     assert_eq!(
       [10.0, 2.0, 3.0].dominance(&[1.0, 2.0, 3.0]),
       Ordering::Greater
@@ -85,13 +102,19 @@ mod tests {
       [1.0, 2.0, 30.0].dominance(&[1.0, 2.0, 3.0]),
       Ordering::Greater
     );
+    assert_eq!(
+      [-2.0, 2.0, -3.0].dominance(&[-1.0, 1.0, 2.0]),
+      Ordering::Greater
+    );
+
     assert_eq!([1.0, 2.0, 3.0].dominance(&[10.0, 2.0, 3.0]), Ordering::Less);
     assert_eq!([1.0, 2.0, 3.0].dominance(&[1.0, 20.0, 3.0]), Ordering::Less);
     assert_eq!([1.0, 2.0, 3.0].dominance(&[1.0, 2.0, 30.0]), Ordering::Less);
-    assert_eq!([1i32; 0].dominance(&[0i32; 0]), Ordering::Equal);
     assert_eq!(
-      [f32::NAN, 20.0, 30.0].dominance(&[1.0, 2.0, 3.0]),
-      Ordering::Equal
+      [-1.0, 2.0, -3.0].dominance(&[2.0, 2.0, 4.0]),
+      Ordering::Less
     );
+
+    assert_eq!([1.0; 0].dominance(&[0.0; 0]), Ordering::Equal);
   }
 }
