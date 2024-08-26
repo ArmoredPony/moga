fb277248ae1153db6ee05ec9844a48dac0f6bfdd
ArmoredPony <raxmetov2003@gmail.com>
2024-08-26 15:22:53 +0400

    rewrite nsga2 solution selection in dod

--- src/optimizer/nsga.rs
+++ src/optimizer/nsga.rs
@@ -1,7 +1,5 @@
 use std::{cmp::Ordering, marker::PhantomData};
 
-use rayon::prelude::*;
-
 use crate::{
   objective::{pareto::ParetoDominance, Scores},
   Crossover,
@@ -18,12 +16,13 @@ pub struct Nsga2<
   const CRS_IN: usize,
   const CRS_OUT: usize,
   Obj: Objectives<OBJ_CNT, S>,
-  Ter: Terminator<OBJ_CNT, S>,
+  Ter: for<'a> Terminator<'a, S, OBJ_CNT>,
   Sel: Selector<S, OBJ_CNT>,
   Crs: Crossover<CRS_IN, CRS_OUT, S>,
   Mut: Mutator<S>,
 > {
-  best_solutions: Vec<(S, Scores<OBJ_CNT>)>,
+  solutions: Vec<S>,
+  scores: Vec<Scores<OBJ_CNT>>,
   new_solutions: Vec<S>,
   initial_population_size: usize,
   objective: Obj,
@@ -35,12 +34,12 @@ pub struct Nsga2<
 }
 
 impl<
-    S: Send,
+    S: Sync,
     const OBJ_CNT: usize,
     const CRS_IN: usize,
     const CRS_OUT: usize,
     Obj: Objectives<OBJ_CNT, S> + Sync,
-    Ter: Terminator<OBJ_CNT, S>,
+    Ter: for<'a> Terminator<'a, S, OBJ_CNT>,
     Sel: Selector<S, OBJ_CNT>,
     Crs: Crossover<CRS_IN, CRS_OUT, S>,
     Mut: Mutator<S>,
@@ -48,43 +47,51 @@ impl<
   for Nsga2<S, OBJ_CNT, CRS_IN, CRS_OUT, Obj, Ter, Sel, Crs, Mut>
 {
   fn run(mut self) -> Vec<S> {
-    self.best_solutions = std::mem::take(&mut self.new_solutions)
-      .into_par_iter()
-      .map(|solution| {
-        let scores = self.objective.evaluate(&solution);
-        (solution, scores)
-      })
+    self.solutions = std::mem::take(&mut self.new_solutions);
+    self.scores = self
+      .solutions
+      .iter()
+      .map(|s| self.objective.evaluate(s))
       .collect();
 
-    while !self.terminator.terminate(&self.best_solutions) {
-      let selected_solutions = self.selector.select(&self.best_solutions);
-      let created_solutions = self.crossover.create(&selected_solutions);
-      let mut created_solutions_scores: Vec<_> = created_solutions
-        .into_iter()
-        .map(|solution| {
-          let scores = self.objective.evaluate(&solution);
-          (solution, scores)
-        })
-        .collect();
-      created_solutions_scores
+    while !self
+      .terminator
+      .terminate(self.solutions.iter().zip(self.scores.iter()))
+    {
+      let mut solutions = std::mem::take(&mut self.solutions);
+      let mut scores = std::mem::take(&mut self.scores);
+
+      let selected_solutions =
+        self.selector.select(solutions.iter().zip(scores.iter()));
+      let mut created_solutions = self.crossover.create(&selected_solutions);
+      created_solutions
         .iter_mut()
-        .for_each(|(s, _)| self.mutator.mutate(s));
-      let mut all_solutions = std::mem::take(&mut self.best_solutions);
-      all_solutions.append(&mut created_solutions_scores);
-      self.best_solutions = self.select_best_solutions(all_solutions);
+        .for_each(|s| self.mutator.mutate(s));
+      let mut created_scores: Vec<_> = created_solutions
+        .iter()
+        .map(|s| self.objective.evaluate(s))
+        .collect();
+
+      solutions.append(&mut created_solutions);
+      scores.append(&mut created_scores);
+
+      let (new_solutions, new_scores) =
+        self.select_best_solutions(solutions, scores);
+      self.solutions = new_solutions;
+      self.scores = new_scores;
     }
 
-    self.best_solutions.into_iter().map(|(s, _)| s).collect()
+    self.solutions
   }
 }
 
 impl<
-    S: Send,
+    S: Sync,
     const OBJ_CNT: usize,
     const CRS_IN: usize,
     const CRS_OUT: usize,
     Obj: Objectives<OBJ_CNT, S> + Sync,
-    Ter: Terminator<OBJ_CNT, S>,
+    Ter: for<'a> Terminator<'a, S, OBJ_CNT>,
     Sel: Selector<S, OBJ_CNT>,
     Crs: Crossover<CRS_IN, CRS_OUT, S>,
     Mut: Mutator<S>,
@@ -93,64 +100,78 @@ impl<
   // really really really want to refactor this mess
   fn select_best_solutions(
     &mut self,
-    population: Vec<(S, Scores<OBJ_CNT>)>,
-  ) -> Vec<(S, [f32; OBJ_CNT])> {
+    solutions: Vec<S>,
+    scores: Vec<Scores<OBJ_CNT>>,
+  ) -> (Vec<S>, Vec<Scores<OBJ_CNT>>) {
+    type SolutionIndex = usize;
+    type DominanceCounter = u32;
+    type CrowdingDistance = f64;
+    type IsBestSolution = bool;
+
     // contains dominated solutions with their indicies by each solution
-    let mut dominance_lists = vec![Vec::new(); population.len()];
+    let mut dominance_lists: Vec<Vec<SolutionIndex>>;
     // contains number of solutions dominating each solution
-    let mut dominance_counters = vec![0; population.len()];
+    let mut dominance_counters: Vec<DominanceCounter>;
+    // contains crowding distance for each solution
+    let mut crowding_distances: Vec<CrowdingDistance>;
     // if a solution belongs to the best ones, the corresponding value is `true`
-    let mut best_solutions_list = vec![false; population.len()];
-    let mut first_front = Vec::new();
+    let mut best_solutions_list: Vec<IsBestSolution>;
+    // indicies of solutions of the first front
+    let mut first_front: Vec<SolutionIndex>;
+
+    dominance_lists = vec![Vec::new(); solutions.len()];
+    dominance_counters = vec![0; solutions.len()];
+    crowding_distances = vec![0.0; solutions.len()];
+    best_solutions_list = vec![false; solutions.len()];
+    first_front = Vec::new();
 
     // fill dominance sets and counters
-    for idx in 0..population.len() {
+    for p_idx in 0..solutions.len() - 1 {
       // for each unique pair of solutions `p`...
-      let (sd, rest) = population[idx..]
-        .split_first()
-        .expect("no solutions remain");
+      let (p_sc, rest_scs) =
+        scores[p_idx..].split_first().expect("no scores remain");
       // and `q`...
-      for (j, other_sd) in rest.iter().enumerate() {
-        let other_idx = idx + j + 1;
-        match sd.1.dominance(&other_sd.1) {
+      for (i, q_sc) in rest_scs.iter().enumerate() {
+        let q_idx = p_idx + i + 1;
+        match p_sc.dominance(q_sc) {
           // if solution `p` dominates solution `q`...
           Ordering::Less => {
             // put solution `q` into list of solutions dominated by `p`
-            dominance_lists[idx].push((other_idx, other_sd));
+            dominance_lists[p_idx].push(q_idx);
             // and increment counter of solutions dominating `q`
-            dominance_counters[other_idx] += 1;
+            dominance_counters[q_idx] += 1;
           }
           // if solution `q` dominates solution `p`...
           Ordering::Greater => {
             // put solution `p` into list of solutions dominated by `q`
-            dominance_lists[other_idx].push((idx, sd));
+            dominance_lists[q_idx].push(p_idx);
             // and increment counter of solutions dominating `p`
-            dominance_counters[idx] += 1;
+            dominance_counters[p_idx] += 1;
           }
           Ordering::Equal => {}
         }
       }
       // if solution `p` isn't dominated by any other solution...
-      if dominance_counters[idx] == 0 {
-        first_front.push((idx, sd, 0.0f64)); // put it into the first front
+      if dominance_counters[p_idx] == 0 {
+        first_front.push(p_idx); // put its index into the first front
       }
     }
 
     let mut last_front = first_front;
     let mut front_solutions_count = last_front.len();
     // while last front isn't empty and we haven't found enough solutions...
-    while !last_front.is_empty() && front_solutions_count < population.len() {
+    while !last_front.is_empty() && front_solutions_count < solutions.len() {
       let mut next_front = Vec::new();
       // for each solution `p` in last front...
-      for (idx, ..) in last_front.iter() {
+      for p_idx in last_front.iter() {
         // for each solution `q` dominated by `p`...
-        for (dommed_idx, dommed, ..) in dominance_lists[*idx].iter_mut() {
+        for q_idx in dominance_lists[*p_idx].iter_mut() {
           // decrement counter of solutions dominating `q`
-          dominance_counters[*dommed_idx] -= 1;
+          dominance_counters[*q_idx] -= 1;
           // if no more solutions dominate `q`...
-          if dominance_counters[*dommed_idx] == 0 {
-            best_solutions_list[*dommed_idx] = true; // mark it as one of the best
-            next_front.push((*dommed_idx, *dommed, 0.0f64)); // and push into next front
+          if dominance_counters[*q_idx] == 0 {
+            best_solutions_list[*q_idx] = true; // mark it as one of the best
+            next_front.push(*q_idx); // and push its index into next front
           }
         }
       }
@@ -158,17 +179,22 @@ impl<
       last_front = next_front;
     }
 
-    // calculate crowding distance for the last found front
-    for obj_idx in 0..OBJ_CNT {
-      last_front.sort_by(|a, b| a.1 .1[obj_idx].total_cmp(&b.1 .1[obj_idx]));
-      let mut first_solution = last_front[0];
-      let mut last_solution = last_front[last_front.len() - 1];
+    // calculate crowding distance for each solution in the last found front
+    // for each objective `o`...
+    for o_idx in 0..OBJ_CNT {
+      // sort solutions by their scores of objective `o`
+      last_front.sort_by(|&a_idx, &b_idx| {
+        scores[a_idx][o_idx].total_cmp(&scores[b_idx][o_idx])
+      });
+      // get the first and the last front members
+      let first_idx = last_front[0];
+      let last_idx = last_front[last_front.len() - 1];
       // set crowding distances of first and last front member to `f64::MAX`
-      first_solution.2 = f64::MAX;
-      last_solution.2 = f64::MAX;
+      crowding_distances[first_idx] = f64::MAX;
+      crowding_distances[last_idx] = f64::MAX;
       // calculate difference between max and min score of current objective
-      let min_score = first_solution.1 .1[obj_idx];
-      let max_score = last_solution.1 .1[obj_idx];
+      let min_score = scores[first_idx][o_idx];
+      let max_score = scores[last_idx][o_idx];
       let score_diff = if max_score - min_score != 0.0 {
         f64::from(max_score - min_score)
       } else {
@@ -176,33 +202,42 @@ impl<
       };
       // for each solution except the first and the last in the last front...
       for solutions in last_front.windows(3) {
-        let prev_sol_score = solutions[0].1 .1[obj_idx]; // cd[i-1]
-        let next_sol_score = solutions[2].1 .1[obj_idx]; // cd[i+1]
-        let mut cur_solution = solutions[1];
+        let (prev_idx, curr_idx, next_idx) =
+          (solutions[0], solutions[1], solutions[2]);
+        let prev_cd = crowding_distances[prev_idx];
+        let next_cd = crowding_distances[next_idx];
         // cd[i] = cd[i] + (cd[i+1] - cd[i-1]) / (max - min)
-        cur_solution.2 +=
-          f64::from(next_sol_score - prev_sol_score) / score_diff
+        crowding_distances[curr_idx] += (next_cd - prev_cd) / score_diff;
       }
     }
 
     // sort solutions in the last front by their crowding distances
-    last_front.sort_by(|a, b| b.2.total_cmp(&a.2));
+    last_front.sort_by(|&a_idx, &b_idx| {
+      crowding_distances[b_idx].total_cmp(&crowding_distances[a_idx])
+    });
     // reset `best solution` flag for each excess solution in the last front
-    for excess in last_front[front_solutions_count - population.len()..].iter()
-    {
-      best_solutions_list[excess.0] = false;
+    for e_idx in last_front[front_solutions_count - solutions.len()..].iter() {
+      best_solutions_list[*e_idx] = false;
     }
 
-    let new_population: Vec<_> = population
+    let (new_sols, new_scs): (Vec<S>, Vec<Scores<OBJ_CNT>>) = solutions
       .into_iter()
+      .zip(scores)
       .zip(best_solutions_list)
-      .filter_map(|(s, is_best)| is_best.then_some(s))
-      .collect();
-    debug_assert_eq!(
-      new_population.len(),
+      .filter_map(|(sol_sc, is_best)| is_best.then_some(sol_sc))
+      .unzip();
+
+    assert_eq!(
+      new_sols.len(),
       self.initial_population_size,
-      "selected population size must match initial population size"
+      "new population size must match initial population size"
     );
-    new_population
+    assert_eq!(
+      new_sols.len(),
+      new_scs.len(),
+      "number of solutions must match number of scores"
+    );
+
+    (new_sols, new_scs)
   }
 }
