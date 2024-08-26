67b24198e6b7cfdad3cc1dda98d6cd89e2dfcf1e
ArmoredPony <raxmetov2003@gmail.com>
2024-08-26 20:14:46 +0400

    add mutators

--- src/mutator.rs
+++ src/mutator.rs
@@ -1,5 +1,42 @@
 /// Mutates solution.
 pub trait Mutator<S> {
   /// Takes a solution and mutates it.
-  fn mutate(&self, solution: &mut S);
+  fn mutate(&mut self, solution: &mut S);
+}
+
+/// `Mutator` that doesn't mutate given value.
+pub struct NoMutator();
+
+impl<S> Mutator<S> for NoMutator {
+  fn mutate(&mut self, _: &mut S) {}
+}
+
+impl<S, F> Mutator<S> for F
+where
+  F: FnMut(&mut S),
+{
+  fn mutate(&mut self, solution: &mut S) {
+    self(solution)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  type Solution = f32;
+
+  fn as_mutator<M: Mutator<Solution>>(_: &M) {}
+
+  #[test]
+  fn test_no_mutator() {
+    let m = NoMutator();
+    as_mutator(&m);
+  }
+
+  #[test]
+  fn test_mutator_from_fn() {
+    let m = |solution: &mut Solution| *solution *= 2.0;
+    as_mutator(&m);
+  }
 }
