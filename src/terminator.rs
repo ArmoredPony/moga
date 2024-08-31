03e68ebf38a784ea1b6e64cdf93e75f6ec479b6e
ArmoredPony <raxmetov2003@gmail.com>
2024-08-31 15:54:22 +0400

    reimplement `terminator` parallelization

--- src/terminator.rs
+++ src/terminator.rs
@@ -1,45 +1,15 @@
-use std::marker::PhantomData;
-
 use rayon::prelude::*;
 
-use crate::{execution::*, score::Scores};
+use crate::{
+  execution::*,
+  operator::{IntoPar, ParBatch, ParEach, TerminationOperatorTag},
+  score::Scores,
+};
 
 /// TODO: add docs
 pub trait TerminationCondition<S, const N: usize> {
   /// TODO: add docs
   fn terminate(&self, solution: &S, scores: &Scores<N>) -> bool;
-
-  /// Creates a wrapper around `Terminator` that marks the given terminator to
-  /// be executed in parallel for **each** solution.
-  ///
-  /// **Parallelization is implemented with [rayon]. As a result, for simple
-  /// tests, parallelization may only decrease performance because of additional
-  /// overhead introduced. Benchmark if in doubt.**
-  fn par_each(self) -> ParEachTerminator<S, N, Self>
-  where
-    Self: Sized,
-  {
-    ParEachTerminator {
-      terminator: self,
-      _solution: PhantomData,
-    }
-  }
-
-  /// Creates a wrapper around `Terminator` that marks the given terminator to
-  /// be executed in parallel for each **batch** of solutions.
-  ///
-  /// **Parallelization is implemented with [rayon]. As a result, for simple
-  /// tests, parallelization may only decrease performance because of additional
-  /// overhead introduced. Benchmark if in doubt.**
-  fn par_batch(self) -> ParBatchTerminator<S, N, Self>
-  where
-    Self: Sized,
-  {
-    ParBatchTerminator {
-      terminator: self,
-      _solution: PhantomData,
-    }
-  }
 }
 
 impl<S, const N: usize, F> TerminationCondition<S, N> for F
@@ -51,6 +21,11 @@ where
   }
 }
 
+impl<S, const N: usize, T> IntoPar<TerminationOperatorTag, S, N> for T where
+  T: TerminationCondition<S, N>
+{
+}
+
 /// Terminates algorithm's execution based on some condition.
 pub trait Terminator<S, const N: usize> {
   /// Takes slices of solutions and their respective scores.
@@ -58,23 +33,12 @@ pub trait Terminator<S, const N: usize> {
   fn terminate(&mut self, solutions: &[S], scores: &[Scores<N>]) -> bool;
 }
 
-pub struct ParEachTerminator<S, const N: usize, T: TerminationCondition<S, N>> {
-  terminator: T,
-  _solution: PhantomData<S>,
-}
-
-pub struct ParBatchTerminator<S, const N: usize, T: TerminationCondition<S, N>>
-{
-  terminator: T,
-  _solution: PhantomData<S>,
-}
-
 // TODO: add docs
-pub trait TerminatorExecutor<ExecutionStrategy, S, const N: usize> {
+pub trait TerminatorExecutor<S, const N: usize, ExecutionStrategy> {
   fn terminate(&mut self, solutions: &[S], scores: &[Scores<N>]) -> bool;
 }
 
-impl<S, const N: usize, T> TerminatorExecutor<CustomExecutionStrategy, S, N>
+impl<S, const N: usize, T> TerminatorExecutor<S, N, CustomExecutionStrategy>
   for T
 where
   T: Terminator<S, N>,
@@ -84,7 +48,7 @@ where
   }
 }
 
-impl<S, const N: usize, T> TerminatorExecutor<SequentialExecutionStrategy, S, N>
+impl<S, const N: usize, T> TerminatorExecutor<S, N, SequentialExecutionStrategy>
   for T
 where
   T: TerminationCondition<S, N>,
@@ -98,8 +62,8 @@ where
 }
 
 impl<S, const N: usize, T>
-  TerminatorExecutor<ParallelEachExecutionStrategy, S, N>
-  for ParEachTerminator<S, N, T>
+  TerminatorExecutor<S, N, ParallelEachExecutionStrategy>
+  for ParEach<TerminationOperatorTag, S, T>
 where
   S: Sync,
   T: TerminationCondition<S, N> + Sync,
@@ -108,23 +72,30 @@ where
     solutions
       .par_iter()
       .zip(scores)
-      .any(|(sol, sc)| self.terminator.terminate(sol, sc))
+      .any(|(sol, sc)| self.operator().terminate(sol, sc))
   }
 }
 
 impl<S, const N: usize, T>
-  TerminatorExecutor<ParallelBatchExecutionStrategy, S, N>
-  for ParBatchTerminator<S, N, T>
+  TerminatorExecutor<S, N, ParallelBatchExecutionStrategy>
+  for ParBatch<TerminationOperatorTag, S, T>
 where
   S: Sync,
   T: TerminationCondition<S, N> + Sync,
 {
   fn terminate(&mut self, solutions: &[S], scores: &[Scores<N>]) -> bool {
-    // FIXME: wrong impl
+    let chunk_size = (solutions.len() / rayon::current_num_threads()).max(1);
     solutions
-      .par_iter()
-      .zip(scores)
-      .any(|(sol, sc)| self.terminator.terminate(sol, sc))
+      .chunks(chunk_size)
+      .zip(scores.chunks(chunk_size))
+      .par_bridge()
+      .any(|chunk| {
+        chunk
+          .0
+          .iter()
+          .zip(chunk.1)
+          .any(|(sol, sc)| self.operator().terminate(sol, sc))
+      })
   }
 }
 
@@ -153,7 +124,7 @@ mod tests {
   fn as_terminator<
     ES,
     const N: usize,
-    T: TerminatorExecutor<ES, Solution, N>,
+    T: TerminatorExecutor<Solution, N, ES>,
   >(
     t: &mut T,
   ) {
